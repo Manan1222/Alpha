a12089b0ecf53970edbce1e3da629d35501870f7
Manan Shah <shahmanan1228@gmail.com>
2017-10-24 14:29:00 +0000

    Update article functionality added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new #created new instance of article
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     #render plain: params[:article].inspect #if we want to show that what values that we passed through the form
     @article = Article.new(article_params) #we need to pass a method so that we can know which article to be created
@@ -15,6 +19,16 @@ class ArticlesController < ApplicationController
     end
     #@article.save
     #redirect_to articles_path(@article) #redirect article to articles/show page
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully Updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   def show
