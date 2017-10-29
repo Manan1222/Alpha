af69fe02ad62ffb8de6b0554d1415d2754d987ff
Manan Shah <shahmanan1228@gmail.com>
2017-10-29 05:02:07 +0000

    Removed foreign Key from create image db

--- db/migrate/20171028132856_create_images.rb
+++ db/migrate/20171028132856_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.string :file_id
-      t.references :articles, index: true, foreign_key: true
+      t.references :articles, index: true
 
       t.timestamps null: false
     end
