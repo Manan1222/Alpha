ebf08526cfb90011781976fafeae1589484409b0
Manan Shah <shahmanan1228@gmail.com>
2017-10-29 04:51:16 +0000

    CHange reference in create image db file

--- db/migrate/20171028132856_create_images.rb
+++ db/migrate/20171028132856_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.string :file_id
-      t.references :post, index: true, foreign_key: true
+      t.references :articles, index: true, foreign_key: true
 
       t.timestamps null: false
     end
